dda11fa569117b49cee5d9cf6ffd17295bb94cde
tony <aamoesi@gmail.com>
2019-11-18 12:50:51 -0500

    commit

---
+++ client/build/precache-manifest.a92ba3747b3a92e529b6657aff413cbb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "1ca94e9fd3c161db96af",
+    "url": "/static/css/main.5d3bbdd7.chunk.css"
+  },
+  {
+    "revision": "1ca94e9fd3c161db96af",
+    "url": "/static/js/main.845e8855.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "f1e7bf602ebeaa757913",
+    "url": "/static/css/2.f3e83ead.chunk.css"
+  },
+  {
+    "revision": "f1e7bf602ebeaa757913",
+    "url": "/static/js/2.77e40145.chunk.js"
+  },
+  {
+    "revision": "bce7e1fecd2a04e784c6107e97862327",
+    "url": "/index.html"
+  }
+];
